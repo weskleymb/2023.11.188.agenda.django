bb7f25e9b4462dc033d9b4bed2c0789d92a025fa
Weskley Bezerra <weskleymb@hotmail.com>
2023-09-14 11:35:09 -0300

    aula django

---
+++ contato/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+def index(request):
+
+    dados = {
+        1: {
+            "nome": "weskley bezerra",
+            "fone": "99675-3679",
+            "email": "weskley@xpto.com"
+        },
+        2: {
+            "nome": "filipe oliveira",
+            "fone": "98787-5972",
+            "email": "filipe@xpto.com"
+        },
+        3: {
+            "nome": "adriana silva",
+            "fone": "98595-5684",
+            "email": "adriana@xpto.com"
+        },
+    }
+
+    return render(request, "index.html", {"contatos": dados})
